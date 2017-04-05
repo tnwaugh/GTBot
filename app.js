d27ac3e69236a664b77d035581bde1a66517240c
tnwaugh <waugh.travis@gmail.com>
2017-04-05 11:07:01 -0400

    Update app.js

--- app.js
+++ app.js
@@ -72,7 +72,9 @@ function updateMessage(input, response) {
 
   var responseText = null;
   var responseAction = null;
-  if (response.output.action === 'display_time') {responseText = 'I understood your intent was '}
+  if (response.output.action === 'display_time') { 'output': {
+        'text': 'The app has not been configured with a <b>WORKSPACE_ID</b> environment variable. Please refer to the ' + '<a href="https://github.com/watson-developer-cloud/conversation-simple">README</a> documentation on how to set this variable. <br>' + 'Once a workspace has been defined the intents may be imported from ' + '<a href="https://github.com/watson-developer-cloud/conversation-simple/blob/master/training/car_workspace.json">here</a> in order to get a working application.'
+      }    }
   if (!response.output) {
     response.output = {};
   } else {
